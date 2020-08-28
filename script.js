b2f7de2dad0e4d7bb48841b4a7b942f917689f5a
hana03 <57242994+hana03@users.noreply.github.com>
2020-08-28 20:28:35 +0900

    portfoliocommit
    portfolioサイトです。

---
+++ script.js
@@ -0,0 +1,63 @@
+$(function(){
+});
+
+  $(function(){
+    $('.menu-btn-sp').on('click', function(){
+      $('.header-nav').toggleClass('is-active');
+    });
+  }());
+
+
+
+  $(function(){
+
+
+$('.mask').click(function(){
+$('.works-lity-wrap').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap').fadeOut();
+}); 
+
+
+$('.mask2').click(function(){
+$('.works-lity-wrap2').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap2').fadeOut();
+});
+
+
+$('.mask3').click(function(){
+$('.works-lity-wrap3').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap3').fadeOut();
+});
+
+
+$('.mask4').click(function(){
+$('.works-lity-wrap4').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap4').fadeOut();
+});
+
+
+$('.mask5').click(function(){
+$('.works-lity-wrap5').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap5').fadeOut();
+});
+
+
+$('.mask6').click(function(){
+$('.works-lity-wrap6').fadeIn();
+});
+$('.fa-times-circle').click(function(){
+  $('.works-lity-wrap6').fadeOut();
+});
+
+
+});
